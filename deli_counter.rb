b0a985a06a07eabddbe8f737a25fb93a0a5557e8
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-20 19:29:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,8 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif 
+    to print = ""
+    
     puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
 
   end
