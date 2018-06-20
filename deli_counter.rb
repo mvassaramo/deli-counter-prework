fc698c64486d54e11b01522b4c65bb74c5ff2e56
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-20 19:45:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
-    elsif 
+    else 
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end
